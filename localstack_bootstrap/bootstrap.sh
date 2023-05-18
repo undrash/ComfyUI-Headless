34b517f7e4fe0d258703137592eaa333c1e7a7b8
Andrei Gaspar <andrei.gaspar@tv2.no>
2023-05-18 14:12:33 +0200

    Add localstack inference topic

--- localstack_bootstrap/bootstrap.sh
+++ localstack_bootstrap/bootstrap.sh
@@ -9,6 +9,7 @@ echo "Bootstrapping Localstack"
 echo "==================="
 LOCALSTACK_HOST=localhost
 AWS_REGION=eu-north-1
+DEFAULT_RESOURCE_NAME="inference"
 
 create_queue() {
     local QUEUE_NAME_TO_CREATE=$1
@@ -20,5 +21,11 @@ create_bucket() {
     awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 s3 mb s3://${BUCKET_NAME_TO_CREATE} --region ${AWS_REGION}
 }
 
-create_queue "inference"
-create_bucket "inference"
+create_sns_topic() {
+    local TOPIC_NAME_TO_CREATE=$1
+    awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sns create-topic --name ${TOPIC_NAME_TO_CREATE}.fifo --region ${AWS_REGION} --attributes ContentBasedDeduplication=true,FifoTopic=true
+}
+
+create_queue $DEFAULT_RESOURCE_NAME
+create_bucket $DEFAULT_RESOURCE_NAME
+create_sns_topic $DEFAULT_RESOURCE_NAME
